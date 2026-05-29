fea9b14d8fe261ed7380af9def98d927ff58047d
Amar Nath <aamarnathkp@gmail.com>
2026-05-29 07:46:13 +0530

    fix: resolve Vercel Node/TS compilation issues and style experience badge & social elements on mobile

--- src/pages/Home/MyDetails.tsx
+++ src/pages/Home/MyDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import { useThemeContext } from "../../providers/ThemeContextProvider";
 import GetImage from "../../components/GetImage/GetImage";
@@ -11,6 +12,30 @@ import {
 } from "./homeElements";
 import Circle from "../../components/Shapes/ShapeCircle";
 import { homeText } from "../../constants/homeSection";
+
+const ExperienceYearText = styled.span<{ $bgColor?: string }>`
+    color: #f7f7f7;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    @media (max-width: 768px) {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        background-color: ${(props) => props.$bgColor};
+        color: #ffffff;
+        width: 2.25rem;
+        height: 2.25rem;
+        border-radius: 50%;
+        padding: 0;
+        margin: 0 0.5rem;
+        font-size: 1.3rem;
+    }
+`;
 
 const MyDetails = () => {
     const themeContext = useThemeContext();
@@ -31,9 +56,9 @@ const MyDetails = () => {
                 <span className='experience-text-description'>
                     {homeText.expDescriptionStart}
                 </span>
-                <span className='experience-year-text'>
-                    {homeText.experience}{" "}
-                </span>
+                <ExperienceYearText $bgColor={themeContext.activeTheme?.primary}>
+                    {homeText.experience}
+                </ExperienceYearText>
                 <span className='experience-text-description'>
                     {homeText.expDescription}
                 </span>
